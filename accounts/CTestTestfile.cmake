5a6e61433c5aa222ee37fdc4012fc84f6a5e535c
Joan CiberSheep <cibersheep@gmail.com>
2017-09-21 01:36:29 +0200

    Add files via upload

---
+++ accounts/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cibersheep/Ubuntu_Touch_Projects/dialer-app/accounts
+# Build directory: /home/cibersheep/Ubuntu_Touch_Projects/dialer-app/accounts
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
